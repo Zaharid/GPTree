1f2988485baa875e5067a26a8eb4a7871e016d75
Zahari <zaharid@gmail.com>
2019-01-29 11:58:43 +0000

    Add test for nfeatures in python

--- test_api.py
+++ test_api.py
@@ -7,12 +7,15 @@ ffi = cffi.FFI()
 ffi.cdef("typedef struct zkdtree  zkdtree;")
 ffi.cdef("zkdtree * zkdtree_load(const char * filename);")
 ffi.cdef("void zkdtree_delete(zkdtree *);")
+ffi.cdef("size_t zkdtree_nfeatures(zkdtree *);")
 ffi.cdef("double zkdtree_interpolate(zkdtree * tree, double * pt);")
 
 
 C = ffi.dlopen(str(pathlib.Path("bld/libcapi.so").absolute()))
 
 tree = C.zkdtree_load(b"bld/tree.cereal")
+
+assert C.zkdtree_nfeatures(tree) == 3
 
 print(C.zkdtree_interpolate(tree, [25., 25., 0.]))
 
